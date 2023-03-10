5357fa67e8d785e018abcb4dfc8497ab97e16f88
Kristi1097 <75994773+Kristi1097@users.noreply.github.com>
2023-03-10 14:50:53 +0100

    Add files via upload

---
+++ Utils/CommandPrompt.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizByKristi.Utils
+{
+    class CommandPrompt
+    {
+        public static void ExecuteCommand(String command)
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo("cmd.exe", "/C" + command)
+                {
+                WindowStyle = ProcessWindowStyle.Hidden,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+                }
+            };
+        process.Start();
+        process.WaitForExit();
+        }
+        public static string ExecuteCommandWithResponse(String command)
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo("cmd.exe", "/C" + command)
+            {
+                WindowStyle = ProcessWindowStyle.Hidden,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            }
+        };
+        process.Start();
+            process.WaitForExit();
+                string result = process.StandardOutput.ReadToEnd();
+                return result;
+        }
+    }
+}
