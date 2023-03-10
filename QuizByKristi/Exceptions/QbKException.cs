2eae58dffe0dcd031af67726745025ff9db273db
Kristi1097 <75994773+Kristi1097@users.noreply.github.com>
2023-03-10 14:49:06 +0100

    Add files via upload

---
+++ QuizByKristi/Exceptions/QbKException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizByKristi.Exceptions
+{
+    class QbKException : Exception
+    {
+        public QbKException()
+        {
+        }
+
+        public QbKException(string message) : base(message)
+        {
+        }
+    }
+}
